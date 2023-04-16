a15830c291f1d2b1056d46ec3ec60d0f92c9fffe
bahalakhzouri <bahaeddine.lakhzouri@esprit.tn>
2023-04-16 17:16:30 +0100

    User update

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ const path = require('path');
 const bodyParser = require('body-parser')
 const userRouter = require('./routes/user.js');
 const cookieParser = require('cookie-parser');
-const cardRouter = require('./routes/cards.js');
 const resultsRouter = require('./routes/results.js');
 const playerRouter = require('./routes/player.js');
 const playersRouter = require('./routes/playersStats.js');
-const truffleConfig = require('./truffle-config');
 const DataRouter = require('./routes/DataRouter.js');
 
 
@@ -24,20 +22,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 const CONNECTION_URL = process.env.CONNECTION_URL;
 
 app.use(cookieParser());
-app.use("/card", cardRouter);
 app.use("/user", userRouter);
 app.use("/player", playerRouter);
 app.use("/players", playersRouter);
 app.use("/results", resultsRouter)
 app.use("/Data", DataRouter)
 
-
-
 const PORT = process.env.PORT || 3001;
-
-app.get('/uploads/:id', (req, res) => {
-  res.sendFile(path.join(__dirname, `./uploads/${req.params.id}`));
-});
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => {
